4134adbf5c2c1ebfa11f01d9fb69b060f67ba7e5
juliascerqueira <juliasscerqueira@outlook.com>
2021-10-05 22:00:57 -0300

    V2

--- exercicio1.js
+++ exercicio1.js
@@ -12,3 +12,17 @@ if(resultado == 10){
 else{
     console.log("Reprovada")
 }
+
+// Código professor
+
+// var resultado = (nota1 + nota2) / 2
+
+// if (resultado >= 7 && resultado < 10){
+//     console.log("Aprovada")
+// }
+// else if(resultado == 10){
+//     console.log("Aprovado com Distinção")
+// }
+// else{
+//     console.log("Reprovada")
+// }
